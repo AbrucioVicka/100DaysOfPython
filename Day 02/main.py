85d692b8cf6db8900fdbe6d729e7e813f38e174a
Vicka Abrucio <vicksa0@gmail.com>
2024-02-18 00:41:05 -0300

    Day 02

---
+++ Day 02/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Tip Calculator!")
+total = float(input("What was the total of the bill?\n $"))
+divide = int(input("How many people to split the bill?\n"))
+percentage = int(input("What percentage would you like to give?\n"))
+split_bill =(total+total*(percentage/100))/divide
+print(f"Each person should pay: $ {round(split_bill,2)}")
