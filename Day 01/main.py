02e1ab751f68e297f054ab43ee039df10ce76768
Vicka Abrucio <vicksa0@gmail.com>
2024-02-17 23:48:09 -0300

    Day 01

---
+++ Day 01/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city=input("\nWhat's the name of the city that you born in?\n")
+pet=input("\nWhat's the name of your pet?\n")
+print("Your perfect band name is: " + city +" " + pet)
