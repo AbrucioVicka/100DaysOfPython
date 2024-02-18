4df6911b9ae41c76bb70aa77fa507c4961b60d6b
Vicka Abrucio <vicksa0@gmail.com>
2024-02-18 10:32:37 -0300

    Day 02 - Altering the formatting of the variable split_bill

--- Day 02/main.py
+++ Day 02/main.py
@@ -3,4 +3,4 @@ total = float(input("What was the total of the bill?\n $"))
 divide = int(input("How many people to split the bill?\n"))
 percentage = int(input("What percentage would you like to give?\n"))
 split_bill =(total+total*(percentage/100))/divide
-print(f"Each person should pay: $ {round(split_bill,2)}")
+print(f"Each person should pay: $ {split_bill:.2f}")
